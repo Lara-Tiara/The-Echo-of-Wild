4473d656aee9a4d49fcad0c3cfcdde9d14320563
Richel Jiang <liangyuli233@163.com>
2023-12-04 13:39:35 +0000

    enable interactable

---
+++ Assets/_Scripts/TextAssetValue.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Object/New Dialog Value", fileName = "New Dialog Asset")]
+public class TextAssetValue : ScriptableObject
+{
+	public TextAsset value;
+
+}
