3247379af9e137c63bc6581a8774326433c1499f
Yang <charlotte1048820903@gmail.com>
2023-12-04 14:35:08 +0000

    update

---
+++ Assets/_Scripts/TextAssetValue.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/New Dialogue Value", fileName = "New Dialogue Asset")]
+public class TextAssetValue : ScriptableObject
+{
+    public TextAsset value;
+}
