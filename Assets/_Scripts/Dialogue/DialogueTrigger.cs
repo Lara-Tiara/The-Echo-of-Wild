95149bf21138f3b2156938431a78aeadeac50ff0
Yang <charlotte1048820903@gmail.com>
2023-12-17 23:04:36 +0800

    update

--- Assets/_Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/_Scripts/Dialogue/DialogueTrigger.cs
@@ -3,34 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class DialogueTrigger : Interactable
+public class DialogueTrigger : MonoBehaviour
 {
     [Header("Que Sign")]
     [SerializeField] private GameObject queSign;
     [Header("Ink JSON")]
     [SerializeField] private TextAsset inkJSON;
+     private bool playerInRange;
+     public UserInput input;
    
     private void Awake() {
+        input = new UserInput();
+        input.Gameplay.Enable();
+        
         playerInRange = false;
+        
         if (queSign == null) {
             Debug.LogError("Que Sign is not assigned in DialogueTrigger.");
             return;
         }
         queSign.SetActive(false);
+        input.Gameplay.Talk.performed += ctx => Talk(ctx);
     }
-    private void Update(){
-            if (queSign == null || inkJSON == null) {
-                return; // Skip the update if either is null
-            }
-            Keyboard kb = InputSystem.GetDevice<Keyboard>();
-            if(playerInRange){
-                queSign.SetActive(true);
-                
-                if(kb.spaceKey.wasPressedThisFrame){
-                    Debug.Log(inkJSON.text);
-                }
-            }else{
-                queSign.SetActive(false);
-            }
+    private void Update() {
+        if (queSign == null || inkJSON == null) {
+            return; // Skip the update if either is null
         }
+        
+        if (playerInRange){
+            queSign.SetActive(true);
+            if (input.Gameplay.Talk.IsPressed()) {
+                Debug.Log("Talk is pressed");
+                //DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+            }
+        }else {
+            queSign.SetActive(false);
+        }
+    }
+
+    public void Talk(InputAction.CallbackContext context) {
+        
+    }
+    public virtual void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            playerInRange = true;
+        }
+    }
+
+    public virtual void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            playerInRange = false;
+        }
+    }
+
 }
--- Assets/_Scripts/Player/PlayerControl.cs
+++ Assets/_Scripts/Player/PlayerControl.cs
@@ -3,28 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerControl : Player
 {
     public Rigidbody2D rb;
-    public UserInput controls;
     private Vector2 moveDirection;
-    private InputAction move;
     [SerializeField] public float moveSpeed = 15f;
     [SerializeField] private Animator animator;
 
-    private void Awake() {
-		rb = this.GetComponent<Rigidbody2D>();
-        controls = new UserInput();
-        if (controls == null) {
-        Debug.LogError("UserInput controls not assigned.");
-        return;
+
+    protected override void Awake() {
+        base.Awake();
+
+        userInput = new UserInput();
+
+        if (userInput == null) {
+                Debug.LogError("UserInput controls not assigned.");
+                return;
         }
-        controls.Gameplay._2DMovement.performed += ctx => _2Dmovement(ctx);
+        rb = this.GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-	}
+    }
+    public void _2Dmovement(InputAction.CallbackContext context) {
+        //Debug.Log("Move");
+    }
+
+    private void OnEnable() {
+        userInput.Gameplay.Talk.performed += Talk;
+        userInput.Gameplay._2DMovement.performed += _2Dmovement;
+    }
+
+    private void OnDisable() {
+        userInput.Gameplay.Talk.performed -= Talk;
+        userInput.Gameplay._2DMovement.performed -= _2Dmovement;
+    }
+
+    private void Talk(InputAction.CallbackContext context) {
+
+    }
     void Update()
     {
-        moveDirection = controls.Gameplay._2DMovement.ReadValue<Vector2>();
+        moveDirection = userInput.Gameplay._2DMovement.ReadValue<Vector2>();
         animator.SetFloat("Horizontal", moveDirection.x);
         animator.SetFloat("Vertical", moveDirection.y);
         animator.SetFloat("Speed", moveDirection.sqrMagnitude);
@@ -33,27 +51,14 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        moveDirection = controls.Gameplay._2DMovement.ReadValue<Vector2>();
+        if (DialogueManager.GetInstance().dialogueIsPlaying)
+        {
+            return;
+        }
+
+        moveDirection = userInput.Gameplay._2DMovement.ReadValue<Vector2>();
         Vector2 pos = transform.position;
 		pos += moveDirection.normalized * moveSpeed * Time.fixedDeltaTime;
         transform.position = pos;
-    }
-
-    public void _2Dmovement(InputAction.CallbackContext context) {
-        //Debug.Log("Move");
-    }
-
-     private void OnEnable() {
-        move = controls.Gameplay._2DMovement;
-        controls.Gameplay.Enable();
-    }
-
-    private void OnDisable() {
-        controls.Gameplay.Disable();
-    }
-    public void Talk(InputAction.CallbackContext context){
-        if(context.phase == InputActionPhase.Performed){
-            Debug.Log("Player want to talk");
-        }
     }
 }
